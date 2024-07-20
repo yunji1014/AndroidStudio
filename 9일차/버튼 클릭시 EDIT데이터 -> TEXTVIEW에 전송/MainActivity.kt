cee3fcebc815586b8585304ae1961b50eba8b67a
yunji1014 <114233449+yunji1014@users.noreply.github.com>
2024-07-21 00:27:25 +0900

    Create MainActivity.kt

---
+++ 9일차/버튼 클릭시 EDIT데이터 -> TEXTVIEW에 전송/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.week2_work_2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+    lateinit var button: Button
+    lateinit var editText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+        editText = findViewById(R.id.editText)
+        button.setOnClickListener {
+            textView.text = editText.text.toString()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
